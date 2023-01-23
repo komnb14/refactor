8c9527b95085178a22137ebc85f176b0741cb715
komnb14 <komnb14@gmail.com>
2023-01-23 19:42:54 +0900

    리팩토링책 다향성에 대하여

---
+++ index.js
@@ -0,0 +1,50 @@
+import {createStatementData} from "./createStatementData.js";
+import {invoices} from "./json.js";
+
+
+function htmlStatement(invoice) {
+    return renderHtml(createStatementData(invoice));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구 내역(고객명 : ${data.customer})\n</h1>`;
+    result += `<table>\n`;
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for (let perf of data.performances) {
+        result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`
+    result += `<p>적립 포인트: <em>${data.totalVolumeCreadits}</em></p>\n`
+    return result;
+}
+
+function statement(invoice) {
+    return renderPlainText(createStatementData(invoice));
+}
+
+function renderPlainText(data) {
+    let result = `청구 내역(고객명 : ${data.customer})\n`;
+
+    for (let perf of data.performances) {
+        result += `${perf.play.name}: ${usd(perf.amount)} (${
+            perf.audience
+        }석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCreadits}점`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+}
+
+const result = statement(invoices[0]);
+
